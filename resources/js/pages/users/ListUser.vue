2c5557e9d8b3cf1336de3048c8ae65caf4060f51
delandapp <alihandronikolas@gmail.com>
2024-07-05 04:31:05 +0700

    add user vue:update

---
+++ resources/js/pages/users/ListUser.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1 class="m-0">Starter List User</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item">
+                            <a
+                                href="https://adminlte-io.translate.goog/themes/v3/starter.html?_x_tr_sl=en&amp;_x_tr_tl=id&amp;_x_tr_hl=id&amp;_x_tr_pto=sc#"
+                                >Home</a
+                            >
+                        </li>
+                        <li class="breadcrumb-item active">Jadwal</li>
+                    </ol>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
